ee6876af1b6c8cff3b86defca98255ae47d23904
Horace Vial <chase.vial@gmail.com>
2026-06-08 16:45:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/budgetapp/SpendingBreakdownActivity.kt
@@ -0,0 +1,11 @@
+package com.example.budgetapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpendingBreakdownActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spending_breakdown)
+    }
+}
